c6efd46e9ba228ef5608d3ef3a50c49ce38665ce
Jinjenia <37193816+Jinjenia@users.noreply.github.com>
2018-03-08 23:41:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,16 +12,16 @@ client.on('message', message => {
 });
 var serverid = "181502086259671040";
 var drole = "267042704603480064";
-bot.on('ready', function() {
-  console.log("Successfully connected autorole: " + bot.username + " - (" + bot.id + ")");
+client.on('ready', function() {
+  console.log("Successfully connected autorole: " + client.username + " - (" + client.id + ")");
 });
 
-bot.on('guildMemberAdd', function(callback) { /* Event called when someone joins the server */
+client.on('guildMemberAdd', function(callback) { /* Event called when someone joins the server */
   if(callback.guild_id == serverid)
-    bot.addToRole({"serverID":serverid,"userID":callback.id,"roleID":drole},function(err,response) {
+    client.addToRole({"serverID":serverid,"userID":callback.id,"roleID":drole},function(err,response) {
       if (err) console.error(err); /* Failed to apply role */
         /* some code */
-	bot.sendMessage({
+	client.sendMessage({
 		to: 181502086259671040,
         message: 'Welcome to the server!'
 	});
