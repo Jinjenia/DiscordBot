375a9ed05b69d7d928566f8b4bd63472943409bd
Jinjenia <37193816+Jinjenia@users.noreply.github.com>
2018-03-08 23:36:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,22 @@ client.on('message', message => {
 		message.reply('pong');
 	}
 });
+var serverid = "181502086259671040";
+var drole = "267042704603480064";
+bot.on('ready', function() {
+  console.log("Successfully connected autorole: " + bot.username + " - (" + bot.id + ")");
+});
+
+bot.on('guildMemberAdd', function(callback) { /* Event called when someone joins the server */
+  if(callback.guild_id == serverid)
+    bot.addToRole({"serverID":serverid,"userID":callback.id,"roleID":drole},function(err,response) {
+      if (err) console.error(err); /* Failed to apply role */
+        /* some code */
+	bot.sendMessage({
+		to: 181502086259671040,
+        message: 'Welcome to the server!'
+	});
+  });
+ });
 
 client.login(process.env.BOT_TOKEN);
