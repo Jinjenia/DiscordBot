fd9b9a74863761e0ce0ed4ca3917bcbd5c2a7b49
Jinjenia <37193816+Jinjenia@users.noreply.github.com>
2018-03-09 20:58:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const music = require('discord.js-music-v11');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -27,20 +26,4 @@ var role = member.guild.roles.find('name', '50cc');
 member.addRole(role)	
 
 });
-const token = "<auth_token>" // Recommended to load from json file.
-
-Bot.on('ready', () => {
-    console.log(`[Start] ${new Date()}`);
-});
-
-music(Bot, {
-	prefix: '-',        // Prefix of '-'.
-	global: false,      // Server-specific queues.
-	maxQueueSize: 10,   // Maximum queue size of 10.
-	clearInvoker: true, // If permissions applicable, allow the bot to delete the messages that invoke it (start with prefix)
-    channel: 'music'    // Name of voice channel to join. If omitted, will instead join user's voice channel.
-});
-Bot.login(BOT_TOKEN);
-
-
 client.login(process.env.BOT_TOKEN);
