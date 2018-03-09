58e371b0b218efa77955fed66a733ecb65129184
Jinjenia <37193816+Jinjenia@users.noreply.github.com>
2018-03-09 21:26:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,11 +20,10 @@ client.on('ready', () => {
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-	console.log('User ' + member.user.username + ' has joined the server!'),
-	message.reply('Welcome to the server ' + member.user.username + ', enjoy your stay!')
+	console.log('User ' + member.user.username + ' has joined the server!')
 	
 var role = member.guild.roles.find('name', '50cc');
-member.addRole(role)	
+member.addRole(role)
 
 });
 client.login(process.env.BOT_TOKEN);
