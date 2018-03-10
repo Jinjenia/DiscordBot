5da77676c0152aefabcdee36d4c303ade1f77c1f
Jinjenia <37193816+Jinjenia@users.noreply.github.com>
2018-03-10 01:29:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log('I am ready!');
+	client.user.setGame('with my dick');
 });
-client.user.setGame('with my dick')
 
 client.on('message', message => {
 	if (message.content === 'dalen') {
